afe01aa92517cb71491aca4e1b217bc6db825cc4
goonmandu <hwang.mingi7@gmail.com>
2022-09-19 16:56:17 -0400

    Fix fibonacci sequence

--- homework/hw1/part5/main.cpp
+++ homework/hw1/part5/main.cpp
@@ -13,27 +13,9 @@ int main() {
     double diff = fabs(max - (int) max);
     double newmax = (int) (max * 100) / (double) 100;
     if (max > -1) {
-        /*
-        std::cout << diff << ", " << ((int) (diff * 100)) % 10 << std::endl;
-        if (diff < 0.01) {
-            std::cout << "Maximum recorded temperature is " << newmax << ".00°C" << std::endl;
-        } else if (((int) (diff * 100)) % 10 < 0.01) {
-            std::cout << "Maximum recorded temperature is " << newmax << "0°C" << std::endl;
-        } else {
-            std::cout << "Maximum recorded temperature is " << newmax << "°C" << std::endl;
-        }
-         */
-       
         std::cout.setf(std::ios::fixed);
         std::cout.precision(2);
         std::cout << max << std::endl;
-
-        /*
-        int ipart = (int) max;
-        int tenspart = round((max - ipart) * 10);
-        int hundpart = round((max - ipart - tenspart / 10.0) * 100);
-        std::cout << "Maximum recorded temperature is " << ipart << "." << tenspart << hundpart << "°C" << std::endl;
-         */
     } else {
         std::cout << "No valid temperature was recorded." << std::endl;
     }
--- labs/lab_3/part5/main.cpp
+++ labs/lab_3/part5/main.cpp
@@ -3,14 +3,18 @@
 long long int fib(int num) {
     if (num < 2) {
         return num;
-    } else {
-        return fib(num - 1) + fib(num - 2);
     }
+    long long int i1 = 0;
+    long long int i2 = 1;
+    long long int n;
+    for (int i = 0; i <= num - 2; i++) {
+        n = i1 + i2;
+        i1 = i2;
+        i2 = n;
+    }
+    return n;
 }
 
 int main() {
-    int in;
-    std::cout << "Enter a number: ";
-    std::cin >> in;
-    std::cout << fib(in) << std::endl;
+    std::cout << fib(10) << " " << fib(20) << " " << fib(30) << " " << fib(40) << " " << fib(50) << std::endl;
 }
