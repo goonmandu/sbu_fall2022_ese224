d3681290faf89ffa6169bff9b66de290ccb560d8
goonmandu <hwang.mingi7@gmail.com>
2022-09-19 10:48:05 -0400

    Enable Lab 2, Part 4

--- labs/lab_2/part4and5/main.cpp
+++ labs/lab_2/part4and5/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "Line.h"
 
-
-
 int main() {
     double m, b;
-    /*
+
     std::cout << "Slope and y-intercept: ";
     std::cin >> m >> b;
     Line l1 = Line(m, b);
@@ -22,12 +20,9 @@ int main() {
     std::cout << "Incrementing y-intercept by 6.9" <<std::endl;
     l2 + 6.9;
     l2.print_attribs();
-     */
+    std::cout << "Are the two lines equal after modifications: " << (l1 == l2) << std::endl;
 
-    
     Line l3 = Line(0.5, -0.5);
     Rectangle r1 = Rectangle(2, 1, 3, 2);
-    std::cout << l3.intersects(r1) << std::endl;
-
-    // std::cout << "Are the two lines equal after modifications: " << (l1 == l2) << std::endl;
+    std::cout << "Does l3 intersect r1? " << l3.intersects(r1) << std::endl;
 }
