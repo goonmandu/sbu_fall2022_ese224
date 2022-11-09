2c916d98d37228e96a5309b960bc28403e14b0d9
goonmandu <hwang.mingi7@gmail.com>
2022-11-09 17:10:34 -0500

    Fix consistent indentation and remove commented code

--- projects/midterm_beta/Library.h
+++ projects/midterm_beta/Library.h
@@ -2,8 +2,6 @@
 #include <vector>
 #include <string>
 
-
-
 typedef struct Book {
     long long int isbn;
     std::string title;
@@ -14,39 +12,39 @@ typedef struct Book {
 } Book;
 
 class Library {
-private:
-    std::vector<Book> catalog;
-    int number_of_books;
-    int consec_id;
-public:
-    Library();
-    int num_books();
-    void print_book(Book book);
-    void print_books_vector(std::vector<Book> bookvec);
-    void print_all_books();
-    Book* binary_search_catalog(int id, int start, int end);
-    Book* search_id(int id);
-    Book borrow_book(int id, int borrow_days);
-    void return_book(int id);
-    bool delete_book(int id);
-    void add_book(long long int isbn, std::string title, std::string author, std::string category);
-    void update_day(double days);
-    Book search_catalog_by_id(int id, int start, int end);
-    void operator<<(const Book& book);
-    void sort_alphabetically_ID(std::vector<Book>& vec);
-    void sort_alphabetically_title(std::vector<Book>& vec);
-    void print_book_with_date(Book book);
-    friend std::istream& operator>>(std::istream& in, const Book& book);
-    //liz's 
-    std::vector<Book>searchBook_ISBN(long long int isbn);
-    std::vector<Book>searchBook_title(std::string title);
-    std::vector<Book>searchBook_category(std::string category);
-    std::vector<Book>searchBook_ID(int id);
-    /*
-    std::vector<Book> search_book_title(std::string title);
-    std::vector<Book> search_book_isbn(long long isbn);
-    std::vector<Book> search_book_author(std::string author);
-     */
-    void set_loan_duration(int id, int borrow_days);
-    Book get_last_book();
+    private:
+        std::vector<Book> catalog;
+        int number_of_books;
+        int consec_id;
+    public:
+        Library();
+        int num_books();
+        void print_book(Book book);
+        void print_books_vector(std::vector<Book> bookvec);
+        void print_all_books();
+        Book* binary_search_catalog(int id, int start, int end);
+        Book* search_id(int id);
+        Book borrow_book(int id, int borrow_days);
+        void return_book(int id);
+        bool delete_book(int id);
+        void add_book(long long int isbn, std::string title, std::string author, std::string category);
+        void update_day(double days);
+        Book search_catalog_by_id(int id, int start, int end);
+        void operator<<(const Book& book);
+        void sort_alphabetically_ID(std::vector<Book>& vec);
+        void sort_alphabetically_title(std::vector<Book>& vec);
+        void print_book_with_date(Book book);
+        friend std::istream& operator>>(std::istream& in, const Book& book);
+        //liz's 
+        std::vector<Book>searchBook_ISBN(long long int isbn);
+        std::vector<Book>searchBook_title(std::string title);
+        std::vector<Book>searchBook_category(std::string category);
+        std::vector<Book>searchBook_ID(int id);
+        /*
+        std::vector<Book> search_book_title(std::string title);
+        std::vector<Book> search_book_isbn(long long isbn);
+        std::vector<Book> search_book_author(std::string author);
+        */
+        void set_loan_duration(int id, int borrow_days);
+        Book get_last_book();
 };
